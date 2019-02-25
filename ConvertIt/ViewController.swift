c90c61d5ac42c1155b616bf3c4e360ff1dcffa11
James Steele <jwsteele1@me.com>
2019-02-25 15:04:40 -0500

    complete

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -3,7 +3,7 @@
 //  ConvertIt
 //
 //  Created by James Steele on 2/22/19.
-//  Copyright © 2019 James Steele. All rights reserved.
+//  Copyright © 2019 James Steele. All rights reserved. 
 //
 
 import UIKit
